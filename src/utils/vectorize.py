a99601ad923fedfcb0266e1309d3973733fb8b24
Christian Homeyer <ox182 [at] uni-heidelberg.de>
2020-12-17 18:10:33 +0100

    Refactor

--- src/utils/vectorize.py
+++ src/utils/vectorize.py
@@ -25,38 +25,36 @@ import data as io
 import visu
 
 
-# TODO what are good values for num_topics?
-# TODO not all arguments are passable, this should be a class structure
-def compute_text_vector(tokenized, num_topics=3, method="tfidf"):
-    """
-    Computing feature vectors for tokenized paper abstracts.
-    """
+# TODO docstr
+def compute_tfidf(text, dictionary, return_bow=True):
+    bow_corpus = [dictionary.doc2bow(token) for token in text]
+    tfidf = models.TfidfModel(bow_corpus)
+    corpus_tfidf = tfidf[bow_corpus]
 
-    # TODO does this work only on pandas?
+    if return_bow:
+        return corpus_tfidf, bow_corpus
+    else
+        return corpus_tfidf
 
-    dictionary = corpora.Dictionary(tokenized)
-    BoW_corpus = [dictionary.doc2bow(text) for text in tokenized]
-    tfidf = models.TfidfModel(BoW_corpus)
-    corpus_tfidf = tfidf[BoW_corpus]
+# TODO docstr
+def lsi(corpus, dictionary, num_topics=3):
+    """ """
+    # train model
+    lsi_model = models.LsiModel(
+        corpus, id2word=dictionary, num_topics=num_topics
+    )  
+    # apply model to  document
+    lsi_model[corpus[1]]  
+    return lsi_model
 
-    #    if method == "tfidf":
-    lsi_tfidf = models.LsiModel(
-        corpus_tfidf, id2word=dictionary, num_topics=num_topics
-    )  # train model
-    lsi_tfidf[corpus_tfidf[1]]  # apply model to  document
 
-    #        return (corpus_tfidf, lsi_tfidf)
 
-    #    elif method == "bow":
-    lsi_bow = models.LsiModel(BoW_corpus, id2word=dictionary, num_topics=num_topics)
-    lsi_bow[corpus_tfidf[1]]  # apply model to  document
+# TODO this is kinda uselessly wrapped
+# TODO docstr
+def lda(corpus, dictionary, num_topics=3):
 
-    #        return (BoW_corpus, lsi_bow)
-
-    #    elif method == "lda":
-    # TODO this has so many arguments to pass ...
     lda_model = models.ldamodel.LdaModel(
-        corpus=corpus_tfidf,
+        corpus=corpus,
         id2word=dictionary,
         num_topics=num_topics,
         random_state=100,
@@ -67,53 +65,4 @@ def compute_text_vector(tokenized, num_topics=3, method="tfidf"):
         iterations=100,
         per_word_topics=True,
     )
-
-    #        return (corpus_tfidf, lda_model)
-
-    # TODO: furthur filter and clean data by using functions such as filter_extremes (remove all tokens that are less frequent or more frequent than a number),
-    # filter_n_most_frequent(filter out the ‘remove_n’ most frequent tokens),
-    # merge_with (to merge multiple dictionaries)
-
-    ipdb.set_trace()
-    # TODO look at print statements and get insight
-    # TODO are these features or similarities?
-
-    # Corpus
-    print(dictionary.token2id)  # token -> tokenId.
-    print(dictionary.dfs)  # token_id -> how many documents contain this token.
-    print(BoW_corpus)  # list of (token_id, token_count)
-
-    for doc in corpus_tfidf:
-        print(doc)
-
-    lsi_tfidf.print_topics()
-    lsi_bow.print_topics()
-    lda_model.print_topics()
-
-    visu.plot_2d_space(BoW_corpus, lsi_bow)
-    visu.plot_2d_space(corpus_tfidf, lsi_tfidf)
-    visu.plot_2d_space(corpus_tfidf, lda_model)
-    visu.plot_2d_space(corpus_tfidf, lda_model, use_tsne=True)
-
-
-def test(test_path):
-    """ Testout processing for debugging, etc. """
-    corpus = io.load_json(test_path)
-    corpus_with_token = io.preprocessing(
-        corpus, lib="spacy", stemming=True, lemmatization=True
-    )
-
-    ipdb.set_trace()
-
-    # TODO pass whole corpus or only token columns?
-    compute_text_vector(corpus_with_token, num_topics=3, method="tfidf")
-
-    # TODO debug jessicas try
-    #    vis = pyLDAvis.gensim.prepare(
-    #        lda_model, corpus_tfidf, dictionary=lda_model.id2word, mds="mmds"
-    #    )
-
-
-if __name__ == "__main__":
-    test_path = "data/data_jmlr_vol17.json"
-    test(test_path)
+    return lda_model
