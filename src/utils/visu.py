5d8efee50a224732b18f7890c9eb6d2223718ac1
Christian Homeyer <ox182 [at] uni-heidelberg.de>
2020-12-18 16:23:24 +0100

    Word cloud for tokens. added tsne visu

--- src/utils/visu.py
+++ src/utils/visu.py
@@ -4,82 +4,154 @@
 This contains all relevant visualization routines.
 """
 
-# TODO remove unnecessary dependencies
 import matplotlib.pyplot as plt
 import matplotlib
 
 matplotlib.use("TkAgg")
+from mpl_toolkits.mplot3d import axes3d
 from gensim.utils import simple_preprocess
 from gensim import corpora, models
 from sklearn.manifold import TSNE
 import pyLDAvis.gensim
+from wordcloud import WordCloud
 
 import pandas as pd
 import numpy as np
+import ipdb
 
 
-# TODO
-def project_to_lower(vectors, k_components=2):
+# TODO TEST 3d
+# TODO pass top key words for each topic
+def plot_tsne(corpus, model, dim=2, save_path=None):
     """
-    Given a high dimensional vector, project to lower dimensional space
-    using PCA. This is useful for visualizing high dimensional features and
-    their distances.
-    """
-    raise NotImplementedError()
+    This projects higher dimensional features on a kD space using the TSNE algorithm.
+    t-SNE differs from PCA by preserving only small pairwise distances or
+    local similarities whereas PCA is concerned with
+    preserving large pairwise distances to maximize variance, see:
+    https://towardsdatascience.com/an-introduction-to-t-sne-with-python-example-5a3a293108d1
+    https://www.quora.com/What-advantages-does-the-t-SNE-algorithm-have-over-PCA
 
-
-# TODO make this self contained
-# TODO make this more flexible to take just numpy feature vectors
-# TODO refactor projection
-# TODO make save option configurable
-def plot_2d_space(corpus, method, use_tsne=False):
-    """
-    This projects higher dimensional features on a kD space using the
-    k largest components in a PCA.
+    arguments:
+    ---
+    corpus []:
+    model []:
     """
 
-    if isinstance(method, models.ldamodel.LdaModel):
-        documents_2d_1 = [x[0][0][1] for x in method[corpus]]
-        documents_2d_2 = [x[0][1][1] for x in list(method[corpus])]
-    else:
-        documents_2d_1 = [x[0][1] for x in method[corpus]]
-        documents_2d_2 = [x[1][1] for x in list(method[corpus])]
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-
+    # TODO this cant be vectorized?
     # Get topic weights
     topic_weights = []
-    for i, row_list in enumerate(method[corpus]):
-        if isinstance(method, models.ldamodel.LdaModel):
+    for i, row_list in enumerate(model[corpus]):
+        if isinstance(model, models.ldamodel.LdaModel):
             topic_weights.append([w for i, w in row_list[0]])
         else:
             topic_weights.append([w for i, w in row_list])
 
     # Array of topic weights
     arr = pd.DataFrame(topic_weights).fillna(0).values
-
     # Dominant topic number in each doc
     topic_num = np.argmax(arr, axis=1)
 
-    if use_tsne:
-        tsne_model = TSNE(
-            n_components=2, verbose=1, random_state=0, angle=0.99, init="pca"
-        )
-        tsne = tsne_model.fit_transform(arr)
-        documents_2d_1 = tsne[:, 0]
-        documents_2d_2 = tsne[:, 1]
+    tsne_model = TSNE(
+        n_components=dim, verbose=1, random_state=0, angle=0.99, init="pca"
+    )
+    tsne = tsne_model.fit_transform(arr)
 
-    ax.scatter(documents_2d_1, documents_2d_2, c=topic_num, s=80, alpha=0.8)
-    #    print(corpus["title"])
-    for i, data in enumerate(corpus):
-        ax.annotate(i, (documents_2d_1[i] + 0.01, documents_2d_2[i]))
+    if dim == 2:
+        fig, ax = plt.subplots(figsize=(10, 10))
+    elif dim == 3:
+        fig, ax = plt.subplots(111, figsize=(10, 10), projection="3d")
+    ax.scatter(*zip(*tsne), c=topic_num, s=80, alpha=0.8)
+    # TODO include labels for cluster that show their topics
+    # How to get top key words for num topics
+    ipdb.set_trace()
+    ax.legend()
 
-    plt.show()
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
+        plt.close()
+    else:
+        plt.show()
 
 
-def plot_segmentation():
+def plot_lda(corpus, model, dim=2):
+
+    # TODO why is this only 2d?
+    # TODO could we check for 3d?
+    projection = []
+    for i in range(dim):
+        if isinstance(model, models.ldamodel.LdaModel):
+            # TODO why list(model[corpus]) for i > 0?
+            projection.append([x[0][i][1] for x in model[corpus]])
+        else:
+            projection.append([x[i][1] for x in model[corpus]])
+
+    #    if dim == 2:
+    #        if isinstance(model, models.ldamodel.LdaModel):
+    #            documents_2d_1 = [x[0][0][1] for x in model[corpus]]
+    #            documents_2d_2 = [x[0][1][1] for x in list(model[corpus])]
+    #        else:
+    #            documents_2d_1 = [x[0][1] for x in model[corpus]]
+    #            documents_2d_2 = [x[1][1] for x in list(model[corpus])]
+
+    # Get topic weights
+    topic_weights = []
+    for i, row_list in enumerate(model[corpus]):
+        if isinstance(model, models.ldamodel.LdaModel):
+            topic_weights.append([w for i, w in row_list[0]])
+        else:
+            topic_weights.append([w for i, w in row_list])
+
+    # Array of topic weights
+    arr = pd.DataFrame(topic_weights).fillna(0).values
+    # Dominant topic number in each doc
+    topic_num = np.argmax(arr, axis=1)
+
+    if dim == 2:
+        fig, ax = plt.subplots(figsize=(10, 10))
+    elif dim == 3:
+        fig, ax = plt.subplots(111, figsize=(10, 10), projection="3d")
+    else:
+        raise Exception("Higher dimensions not supported")
+
+    ax.scatter(*zip(*tsne), c=topic_num, s=80, alpha=0.8)
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
+        plt.close()
+    else:
+        plt.show()
+
+
+# TODO this needs a projection similar to tsne
+def plot_segmentation(data, label):
     """
     Given assignments and data points, color the different cluster and plot them in a
     2D space.
     """
     raise NotImplementedError()
+
+
+def plot_wordcloud(text, save_path=None):
+    """ Plot word cloud on joined text using frequeny count """
+
+    if isinstance(text, pd.Series):
+        text = text.str.join(" ")
+        text = " ".join(text.tolist())
+    elif isinstance(text, list):
+        text = " ".join(text)
+    else:
+        raise Exception("Please pass list or pd.Series")
+
+    # wordcloud = WordCloud(max_font_size=60, max_words=100, background_color="white").generate_from_frequencies(df_counter_words)
+    wordcloud = WordCloud(
+        max_font_size=60, max_words=100, background_color="white", collocations=False
+    ).generate(text)
+
+    fig = plt.figure(1, figsize=(12, 12))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
+        plt.close()
+    else:
+        plt.show()
