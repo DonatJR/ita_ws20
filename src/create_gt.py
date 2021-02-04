9625e8b1ace9fa9ddb3ae98658302e6681187e3f
Christian Homeyer <ox182 [at] uni-heidelberg.de>
2021-02-04 13:59:25 +0100

    Update

--- src/create_gt.py
+++ src/create_gt.py
@@ -18,6 +18,7 @@ import scipy
 
 import utils.visu as visu
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 """
 We want to create ground truth labels for evaluation and supervision.
@@ -34,7 +35,7 @@ def convert_list_to_str(x):
     return " ".join(x)
 
 
-def naive(data):
+def naive(data, return_val="distance"):
     """
     Go over all key words and create global array.
     Query each key word for each paper. Mark every key words that is used for the
@@ -73,62 +74,82 @@ def naive(data):
     data = data.apply(flatten)
     set_kwords = get_global_key_word_vect(data)
     features = compute_vectors(data, set_kwords)
-    cluster = DBSCAN(
-        # NOTE eps is very sensitive. if set too high we get easily no clusters
-        eps=0.0001,
-        min_samples=2,
-        metric=scipy.spatial.distance.hamming,
-        n_jobs=4,
-    ).fit(features)
-    #    D = sklearn.metrics.pairwise_distances(
-    #        features, metric=scipy.spatial.distance.hamming
-    #    )
-    ipdb.set_trace()
-    return cluster.labels_
+    if return_val == "distance":
+        D = sklearn.metrics.pairwise_distances(
+            features, metric=scipy.spatial.distance.hamming
+        )
+        return D
+    else:
+        cluster = DBSCAN(
+            # NOTE eps is very sensitive. if set too high we get easily no clusters
+            eps=0.002,
+            min_samples=2,
+            metric=scipy.spatial.distance.hamming,
+            n_jobs=4,
+        ).fit(features)
+        return cluster.labels_
 
 
-def cluster_naive(data):
-    pass
+def visualize_distances(D):
+    sns.displot(D, bins=70, kde=True)
+    #    plt.title("Distribution of pairwise binary key word vector hamming distances")
+    #    plt.xlabel("Key word Hamming distance")
+    #    plt.ylabel("#")
+    plt.show()
 
 
 def visualize_clusters():
     pass
 
 
-# TODO show top key words with number
-def visualize_key_words(data):
+def visualize_key_words(data, return_items=True):
     """ Plot histogram of key words to count """
     data = data.apply(flatten)
     data = data.apply(convert_list_to_str)
-    #    counts = Counter(" ".join(data.values.tolist()).split(" ")).items()
-    counts = Counter(" ".join(data.values.tolist()).split(" ")).values()
+    counts = Counter(" ".join(data.values.tolist()).split(" "))
+    del counts[""]
+    counts = counts.items()
 
     fig = plt.figure()
-    plt.hist(counts, bins=100)
+    sns.displot(counts.values(), kde=True)
     plt.title("Distribution of counts for key words over dataset")
     plt.show()
 
+    if return_items:
+        return counts
+
 
 # TODO how many different key words do we have before and after preprocessing?
-# TODO how many clusters would we get with the naive way?
 def main():
     #    test_path = Path("data/data_jmlr_vol13-21.json")
     #    corpus = io.load_json(test_path, return_data="keywords")
+
     # NOTE this preprocessing will tokenize the key words. This way we can capture
     # similar key words to be the same thing
+    # TODO Problem: if we separate key words, the meaning can change drastically
+    # e.g. "dynamic programming" -> "dynamic", "programming" has a complete different semantic meaning
+
     #    corpus = io.preprocessing(
     #        corpus,
     #        lib="spacy",
     #        datatype="keywords",
-    #        stemming=True,
+    #        stemming=False,
     #        lemmatization=True,
     #    )
 
     # NOTE with preprocessing we will have 1754 key words in total
     corpus = helper.read_pickle("data/tokenized_kwords_data_jmlr_vol13-21.pkl")
+    kword_counts = visualize_key_words(corpus)
     ipdb.set_trace()
-    #    visualize_key_words(corpus)
-    cluster = naive(corpus)
+    print(kword_counts.most_common(20))
+
+    # NOTE for the first ~500 kwords do we have several counts
+    # this could actually result in few clusters based on distance
+    # TODO problem: if we naively cluster all keyy words contribute equally to distance
+    # but some words are more descriptive than others, so a weighted contribution would be intuitive
+    # this is however crazy subjective and the core problem
+    D = naive(corpus, return_val="distance")
+    visualize_distances(np.ravel(D))
 
 
 if __name__ == "__main__":
