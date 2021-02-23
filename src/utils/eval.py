bca42bfd54cb5ab0785b7fbc871f311c7e27b452
JessicaKaechele <jessica.kaechele@web.de>
2021-02-23 22:38:02 +0100

    implement evaluation into pipeline

--- src/utils/eval.py
+++ src/utils/eval.py
@@ -1,5 +1,5 @@
 """
-Evaluation pipeline for evaluating clustering algorithms on research papers. 
+Evaluation pipeline for evaluating clustering algorithms on research papers.
 """
 
 import numpy as np
@@ -11,10 +11,14 @@ class Evaluation:
     Wrapper class for sklearn metrics.
     """
 
-    def __init__(self, features, prediction, groundtruth=None):
+    def __init__(self, features, corpus):
         self.features = features
-        self.prediction = prediction
-        self.groundtruth = groundtruth
+        self.prediction = corpus["predicted_labels"]
+
+        if "label" in corpus:
+            self.groundtruth = corpus["label"]
+        else:
+            self.groundtruth = None
 
     def __len__(self):
         return len(self.prediction)
@@ -42,7 +46,7 @@ class Evaluation:
                 contingency_matrix
             )
 
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
@@ -51,7 +55,7 @@ class Evaluation:
             return purity_score(self.groundtruth, self.prediction)
 
     def adjusted_rand_score(self):
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
@@ -60,7 +64,7 @@ class Evaluation:
             return metrics.adjusted_rand_score(self.groundtruth, self.prediction)
 
     def adjusted_mutual_info_score(self):
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
@@ -70,40 +74,64 @@ class Evaluation:
 
     def precision(self):
         """ NOTE: this can be dangerous as we may not have correct classifications, but just partitions. """
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
                 self.prediction
             ), "Groundtruth and predictions must have same length"
-            return metrics.precision_score(self.groundtruth, self.prediction)
+            return metrics.precision_score(
+                self.groundtruth,
+                self.prediction,
+                average="weighted",
+                zero_division=True,
+            )
 
     def recall(self):
         """ NOTE: this can be dangerous as we may not have correct classifications, but just partitions. """
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
                 self.prediction
             ), "Groundtruth and predictions must have same length"
-            return metrics.recall_score(self.groundtruth, self.prediction)
+            return metrics.recall_score(
+                self.groundtruth, self.prediction, average="weighted"
+            )
 
     def f1(self):
         """ NOTE: this can be dangerous as we may not have correct classifications, but just partitions. """
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
                 self.prediction
             ), "Groundtruth and predictions must have same length"
-            return metrics.f1_score(self.groundtruth, self.prediction)
+            return metrics.f1_score(
+                self.groundtruth, self.prediction, average="weighted"
+            )
 
     def accuracy(self):
         """ NOTE: this can be dangerous as we may not have correct classifications, but just partitions. """
-        if not self.groundtruth:
+        if self.groundtruth is None:
             raise Exception("Supervised metric needs groundtruth")
         else:
             assert len(self.groundtruth) == len(
                 self.prediction
             ), "Groundtruth and predictions must have same length"
             return metrics.accuracy_score(self.groundtruth, self.prediction)
+
+    def evaluate_all(self):
+        all_scores = {}
+        all_scores["silhouette_score"] = self.silhouette_score()
+        all_scores["calinski_harabasz"] = self.calinski_harabasz()
+        all_scores["davies_bouldin_score"] = self.davies_bouldin_score()
+        if self.groundtruth is not None:
+            all_scores["purity"] = self.purity()
+            all_scores["adjusted_rand_score"] = self.adjusted_rand_score()
+            all_scores["adjusted_mutual_info_score"] = self.adjusted_mutual_info_score()
+            all_scores["precision"] = self.precision()
+            all_scores["recall"] = self.recall()
+            all_scores["f1"] = self.f1()
+            all_scores["accuracy"] = self.accuracy()
+        return all_scores
