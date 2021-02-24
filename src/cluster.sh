3c162c72691c067f971d6c0a8ada18b43e67b8ec
Daniela Fichiu <daniela.fichiu@stud.uni-heidelberg.de>
2021-02-24 03:06:50 +0100

    add bash script for running experiments with all configs and different dim_reduction/n_components

---
+++ src/cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare -a arr=("lsa" "spectral" "normal")
+
+for file in ./configurations/*
+do
+
+    echo "----- Clustering with configs $file -----"
+    for red in "${arr[@]}"
+    do  
+        # if you are not on OS X, remove '' -e
+        echo "----- dimensionality_reduction:  $red -----"
+        sed -i '' -e "s/dimensionality_reduction:.*/dimensionality_reduction: $red/g" $file
+
+        for n in {2..25}
+        do
+            # if you are not on OS X, remove '' -e
+            echo "----- n_components: $n -----"
+            sed -i '' -e "s/n_components:.*/n_components: $n/g" $file
+            python3 main.py --config $file
+        done 
+    done
+done
