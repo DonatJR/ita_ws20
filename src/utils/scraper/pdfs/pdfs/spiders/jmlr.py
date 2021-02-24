a2a051a1e3c58a5765543073a0882ee9e4a8c5ff
JessicaKaechele <jessica.kaechele@web.de>
2021-02-24 14:01:50 +0100

    reformat code

--- src/utils/scraper/pdfs/pdfs/spiders/jmlr.py
+++ src/utils/scraper/pdfs/pdfs/spiders/jmlr.py
@@ -1,40 +1,38 @@
+import logging
+import logging.config
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+
 from ..items import PdfsItem
-import logging.config
-import logging
 
 # Init logger
-logging.config.fileConfig(fname='logging.conf')
-slogger = logging.getLogger('stats')
+logging.config.fileConfig(fname="logging.conf")
+slogger = logging.getLogger("stats")
 
 
 class JmlrSpider(CrawlSpider):
-    name = 'jmlr'
-    allowed_domains = ['jmlr.csail.mit.edu']
-    start_urls = ['https://jmlr.csail.mit.edu/']
+    name = "jmlr"
+    allowed_domains = ["jmlr.csail.mit.edu"]
+    start_urls = ["https://jmlr.csail.mit.edu/"]
 
-
-    rules = (
-        Rule(LinkExtractor(allow=r'papers/'), callback='parse_item', follow=True),
-    )
+    rules = (Rule(LinkExtractor(allow=r"papers/"), callback="parse_item", follow=True),)
 
     def parse_item(self, response):
-       file_url = response.css('a[target ="_blank"]::attr(href)').extract()
+        file_url = response.css('a[target ="_blank"]::attr(href)').extract()
 
-       if file_url == []:
+        if file_url == []:
             file_url = response.css('a[id ="pdf"]::attr(href)').extract()
 
-       if file_url != []:
+        if file_url != []:
             if not file_url[0].startswith("http"):
                 file_url = "https://jmlr.csail.mit.edu/" + file_url[0]
             else:
                 file_url = file_url[0]
-            
+
             slogger.info(file_url)
 
             item = PdfsItem()
-            item['file_urls'] = [file_url]
+            item["file_urls"] = [file_url]
             yield item
-       
